9db67f6ddd86e24e06a45152e2966257967eeae9
zhy <794182811@qq.com>
2019-06-06 20:26:03 +0800

    消息转发

---
+++ study-demo-hystrix/src/main/java/org/study/demo/hystrix/controller/HelloController.java
@@ -0,0 +1,25 @@
+package org.study.demo.hystrix.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.study.demo.hystrix.service.HelloService;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@RestController
+public class HelloController {
+	   @Autowired
+	    HelloService helloService;
+
+	   @HystrixCommand(fallbackMethod = "hiError")
+	    @GetMapping(value = "/hi")
+	    public String hi(@RequestParam String name) {
+	        return helloService.hiService( name );
+	    }
+	   
+	   public String hiError(String name) {
+	        return "hi,"+name+",sorry,error!";
+	    }
+}
---
+++ study-demo-stream2/src/main/java/org/study/demo/stream2/App.java
@@ -0,0 +1,13 @@
+package org.study.demo.stream2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
