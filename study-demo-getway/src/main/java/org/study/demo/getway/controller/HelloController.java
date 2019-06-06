d14eeb0407fdab2bffbce38316d4f73d937a03d4
zhy <794182811@qq.com>
2019-06-06 17:06:03 +0800

    加入了熔断机制

--- study-demo-getway/src/main/java/org/study/demo/getway/controller/HelloController.java
+++ study-demo-getway/src/main/java/org/study/demo/getway/controller/HelloController.java
@@ -6,13 +6,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.study.demo.getway.service.HelloService;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 public class HelloController {
 	   @Autowired
 	    HelloService helloService;
 
+	   @HystrixCommand(fallbackMethod = "hiError")
 	    @GetMapping(value = "/hi")
 	    public String hi(@RequestParam String name) {
 	        return helloService.hiService( name );
+	    }
+	   
+	   public String hiError(String name) {
+	        return "hi,"+name+",sorry,error!";
 	    }
 }
